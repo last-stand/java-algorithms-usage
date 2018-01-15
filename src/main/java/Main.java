1f0040f788faa2217e706391f3f28bc07245fa55
Jai Prakash <jaiprak@thoughtworks.com>
2018-01-15 20:54:20 +0530

    AES Encryption user defined secret key with hash, passing as secret. | Jai

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,14 @@
 import encryption.AES;
 
-import javax.crypto.SecretKey;
-
 public class Main {
     public static void main(String[] args) throws Exception{
-
-        // AES Encryption Algorithm
-        SecretKey secretKey = AES.getSecretEncryptionKey();
-        System.out.println("SecretKey: "+ secretKey);
+        // AES encryption
+        final String secretKey = "abcd1234";
+        String originalString = "Hello World!";
         byte[] iv = AES.generateInitializationVector();
-        String encryptedText = AES.encryptText(secretKey, "Hello world", iv);
+        String encryptedText = AES.encrypt(secretKey, originalString, iv);
         System.out.println("EncryptedText: " + encryptedText);
-        String decryptedText = AES.decryptText(secretKey, encryptedText, iv);
+        String decryptedText = AES.decrypt(secretKey, encryptedText, iv);
         System.out.println("DecryptedText: " + decryptedText);
     }
 }
--- src/main/java/encryption/AES.java
+++ src/main/java/encryption/AES.java
@@ -1,23 +1,37 @@
 package encryption;
 
-import com.sun.tools.javac.util.Convert;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 public class AES {
     final static int AES_KEYLENGTH = 128;
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
 
-    public static SecretKey getSecretEncryptionKey() throws Exception{
-        KeyGenerator generator = KeyGenerator.getInstance("AES");
-        generator.init(AES_KEYLENGTH);
-        SecretKey secKey = generator.generateKey();
-        return secKey;
+    public static void setKey(String secret)
+    {
+        try {
+            key = secret.getBytes("UTF-8");
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
     public static byte[] generateInitializationVector(){
@@ -27,20 +41,37 @@ public class AES {
         return iv;
     }
 
-    public static String encryptText(SecretKey secretKey, String strDataToEncrypt, byte[] iv) throws Exception{
-        Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        aesCipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
-        byte[] byteDataToEncrypt = strDataToEncrypt.getBytes();
-        byte[] byteCipherText = aesCipher.doFinal(byteDataToEncrypt);
-        String strCipherText = new BASE64Encoder().encode(byteCipherText);
-        return strCipherText;
+    public static String encrypt(String secret, String strDataToEncrypt, byte[] iv) {
+        try
+        {
+            setKey(secret);
+            Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            aesCipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
+            byte[] byteDataToEncrypt = strDataToEncrypt.getBytes();
+            byte[] byteCipherText = aesCipher.doFinal(byteDataToEncrypt);
+            String strCipherText = new BASE64Encoder().encode(byteCipherText);
+            return strCipherText;
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Error while encrypting: " + ex.toString());
+        }
+        return null;
     }
 
-    public static String decryptText(SecretKey secretKey, String cipherText, byte[] iv) throws Exception{
-        Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        aesCipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
-        byte[] byteCipherText = new BASE64Decoder().decodeBuffer(cipherText);
-        byte[] byteDecryptedText = aesCipher.doFinal(byteCipherText);
-        return new String(byteDecryptedText);
+    public static String decrypt(String secret, String cipherText, byte[] iv) {
+        try {
+            setKey(secret);
+            Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            aesCipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+            byte[] byteCipherText = new BASE64Decoder().decodeBuffer(cipherText);
+            byte[] byteDecryptedText = aesCipher.doFinal(byteCipherText);
+            return new String(byteDecryptedText);
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Error while decrypting: " + ex.toString());
+        }
+        return null;
     }
 }
